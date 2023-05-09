4b28fe7ae5d6a4f73151521b9c0c672d8382aa47
DESKTOP-8N3GG4O\GGG <zalwjdz29@naver.com>
2023-05-09 15:33:09 +0900

    JPA 설정, 테이블 생성 및 테스트

---
+++ src/main/java/com/tenco/blog/model/Board.java
@@ -0,0 +1,54 @@
+package com.tenco.blog.model;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity 
+public class Board {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false, length = 100)
+	private String title;
+	
+	@Lob // 대용량 데이터 설정 
+	private String content;
+	
+	@ColumnDefault("0") 
+	private int count;
+	
+	// Board와 User 관계는 N : 1
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	// 리플에 대한 정보가 없음 !!! 
+	// Board 정보를 가지고 올 때 댓글 정보도 가지고 와야 된다면!!!
+	// Board 와 Reply의 관계는 1 : N
+	@OneToMany(mappedBy = "board", fetch = FetchType.LAZY) // LAZY -> 기본값 
+	private List<Reply> reply;
+	// 주의 : Board 테이블에 reply_id 컬럼이 필요한가(X)
+	// 그때 설정하는 것 -> mappedBy
+	// mappedBy 설정하면 board 테이블에 컬럼이 생성되지 않는다. 
+	// 오브젝트가 생성될 때 즉 데이터를 가지고 올 때 알아서 join 처리 
+	// 해서 데이터만 가지고 와라 
+	
+	@CreationTimestamp // now()
+	private Timestamp createdDate;
+}
---
+++ src/main/java/com/tenco/blog/model/Reply.java
@@ -0,0 +1,38 @@
+package com.tenco.blog.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+public class Reply {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false, length = 250)
+	private String content;
+	
+	// 어느 게시글에 작성된 리플인지 
+	// Reply 와 board 연관 관계 - N : 1
+	@ManyToOne
+	@JoinColumn(name = "board_id")
+	private Board board;
+	
+	// 누가 작성한 리플인지
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@CreationTimestamp
+	private Timestamp createdData; 
+}
