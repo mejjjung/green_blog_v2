1c994a3ee745f38430f665fef569a860866799fb
DESKTOP-8N3GG4O\GGG <zalwjdz29@naver.com>
2023-05-11 14:14:14 +0900

    로그아웃 기능 구현

--- src/main/java/com/tenco/blog/controller/UserController.java
+++ src/main/java/com/tenco/blog/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.tenco.blog.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
+	@Autowired
+	private HttpSession session;
+	
 	// 로그인 페이지
 	@GetMapping("/loginPage")
 	public String loginPage() {
@@ -18,5 +24,13 @@ public class UserController {
 	public String joinPage() {
 		
 		return "/user/join_form";
+	}
+	
+	// 회원가입 처리
+	// 로그아웃
+	@GetMapping("/logout")
+	public String logout() {
+		session.invalidate();
+		return "redirect:/";
 	}
 }
